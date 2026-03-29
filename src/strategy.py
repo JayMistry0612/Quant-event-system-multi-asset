0529b44ac1cef1cae156461855dee6efa75814b0
JayMistry0612 <jayhelly6125@gmail.com>
2026-03-29 16:08:43 +0530

    feat:Strategy changed

--- src/strategy.py
+++ src/strategy.py
@@ -1,7 +1,7 @@
 class Strategy:
     def __init__(self):
         self.prev = {}
-        self.prev_prev = {}
+        
 
     def on_market(self, data,symbol):
 
@@ -9,25 +9,29 @@ class Strategy:
             self.prev[symbol] = data
             return None
 
-        if symbol not in self.prev_prev:
-            self.prev_prev[symbol] = self.prev[symbol]
+        prev=self.prev[symbol]
+        
+        tolerance = 0.005
+        threshold = 0.001
+        
+        ema_20 = prev['EMA_20']
+        ema_50 = prev['EMA_50']
+        price = prev['Close']
+        
+        
+        strength = abs(prev['EMA_20'] - prev['EMA_50']) / prev['Close']
+        
+        if strength < threshold:
             self.prev[symbol] = data
             return None
-
-        prev=self.prev[symbol]
-        prev_prev=self.prev_prev[symbol]
         
-        diff1 = prev['EMA_20'] - prev['EMA_50']       
-        diff2 = prev_prev['EMA_20'] - prev_prev['EMA_50']  
-
-        if diff1 > 0 and diff2 < 0:
+        if ema_20 > ema_50 and price <= ema_20 * (1 + tolerance) and price >= ema_20 * (1 - tolerance):
             signal = 'BUY'
-        elif diff1 < 0 and diff2 > 0:
+        elif ema_20 < ema_50 and price >= ema_20 * (1 - tolerance) and price <= ema_20 * (1 + tolerance):
             signal = 'SELL'
         else:
-            signal = None
-
-        self.prev_prev[symbol] = self.prev[symbol]
+            signal = None    
+        
         self.prev[symbol] = data
 
         return signal
